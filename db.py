7443188a8a3a0247f20629a4f52f76bb89a1ad78
Dave Harrington <784252+DaveHarrington@users.noreply.github.com>
2023-11-26 14:33:30 -0800

    Multi-file, multi-commit diff viewer working (#4)

--- db.py
+++ db.py
@@ -6,6 +6,8 @@ class FileHistory:
         self._orig_file_name = None
         self._current_file_name = None
         self.file_commits = OrderedDict()
+        self.all_patches = OrderedDict()
+        self.total_length = None
 
     def fill(self, commit, file_name=None):
         if not self._orig_file_name:
@@ -13,6 +15,33 @@ class FileHistory:
         self._current_file_name = file_name or self._current_file_name
 
         self.file_commits[commit.sha] = FileCommit(file_name, commit)
+
+    def post_load(self):
+        for i, (commit, fc) in enumerate(self.file_commits.items()):
+            if i < len(self.file_commits):
+                for p in fc.patches:
+                    if p["type"] == "del":
+                        for fc_ in list(self.file_commits.values())[i+1:]:
+                            for p_ in fc_.patches:
+                                if p_["line_start"] > p["line_start"]:
+                                    p_len = p["line_end"] - p["line_start"]
+                                    p_["line_start"] += p_len
+                                    p_["line_end"] += p_len
+
+
+            self.all_patches[commit] = fc.patches
+
+        first_commit = list(self.file_commits.keys())[0]
+        first_len = len(self.file_commits[first_commit].diff_text or "")
+        extra = 0
+        for i, patches in enumerate(self.all_patches.values()):
+            if i == 0:
+                continue
+            for patch in patches:
+                if patch['type'] == 'add':
+                    extra += patch['line_end'] - patch['line_start']
+
+        self.total_length = first_len + extra
 
     def __str__(self):
         return (f"File history: {self._orig_file_name}, "
@@ -26,23 +55,94 @@ class FileCommit:
         self.sha = commit.sha
         self.file_name = file_name
         self.commit = commit
+        self.patches = []
+        self.diff_text = self.load_file_content()
 
-    def get_file_contents(self):
+    def get_content(self, all_patches, total_length):
+        skips = {}
+        seen_our_commit = False
+
+        for commit, patches in all_patches.items():
+            if commit == self.sha:
+                seen_our_commit = True
+            elif not seen_our_commit:
+                for patch in patches:
+                    if patch['type'] == 'del':
+                        skips[patch['line_start']] = (
+                            patch['line_end'] - patch['line_start'])
+            else:
+                for patch in patches:
+                    if patch['type'] == 'add':
+                        skips[patch['line_start']] = (
+                            patch['line_end'] - patch['line_start'])
+
+        text = []
+        diff_pointer = 0
+        while len(text) < total_length:
+
+            skip_len = skips.get(len(text))
+            if skip_len is not None:
+                for j in range(skip_len):
+                    text.append('x')
+                continue
+
+            text.append(self.diff_text[diff_pointer])
+            diff_pointer += 1
+
+        return '\n'.join(text)
+
+    def load_file_content(self):
         parent_commit = self.commit.commit_obj.parents[0]
 
-        diffs = self.commit.commit_obj.diff(
-            parent_commit, paths=self.file_name, create_patch=True,
+        diffs = parent_commit.diff(
+            self.commit.commit_obj,
+            paths=self.file_name,
+            create_patch=True,
             unified=999999,
         )
 
         if diffs:
-            return diffs[0].diff.decode('utf-8')
+            text = diffs[0].diff.decode('utf-8').splitlines()
         else:
             # No diff for this file, get file contents from parent
             parent_blob = parent_commit.tree / self.file_name
+            text = []
             for line in parent_blob.data_stream.read().decode('utf-8').splitlines():
                 # add space for patch format (no change)
-                print(f' {line}')
+                text.append(f' {line}')
+
+        text = text[1:]
+
+        patches = []
+        patch_add = patch_del = None
+        def finish_patch(patch_type, patch_start, line_no):
+            if patch_start is not None:
+                patches.append({
+                    'type': patch_type,
+                    'line_start': patch_start,
+                    'line_end': line_no,
+                })
+            return None
+
+        for line_no, line in enumerate(text):
+            if line.startswith('+'):
+                patch_del = finish_patch('del', patch_del, line_no)
+                if patch_add is None:
+                    patch_add = line_no
+            elif line.startswith('-'):
+                patch_add = finish_patch('add', patch_add, line_no)
+                if patch_del is None:
+                    patch_del = line_no
+            else:
+                patch_add = finish_patch('add', patch_add, line_no)
+                patch_del = finish_patch('del', patch_del, line_no)
+
+        finish_patch('add', patch_add, line_no+1)
+        finish_patch('del', patch_del, line_no+1)
+
+        self.patches = patches
+
+        return text
 
     def __str__(self):
         return f"FileCommit: {self.file_name} @sha: {self.sha}"
@@ -53,13 +153,15 @@ class FileCommit:
 class Commit:
     def __init__(self, commit_obj):
         self.sha = commit_obj.hexsha
-        self.title = commit_obj.summary
+        self.short = commit_obj.hexsha[:8]
+        self.summary = commit_obj.summary
+        self.message = commit_obj.message
         self.changed_files = []
         self.commit_obj = commit_obj
 
     def __str__(self):
         return (f"commit: {self.sha}\n"
-               f"title: {self.title}\n"
+               f"summary: {self.summary}\n"
                f"num files: {len(self.changed_files)}")
 
 class GitData:
@@ -106,9 +208,7 @@ class GitData:
                         if c_inner == commit:
                             break
                         fh.fill(c_inner, None)
-
                     fh.fill(commit, cfile)
-
                     file_histories[cfile.b_path] = fh
 
                 elif cfile.change_type == "M":
@@ -121,6 +221,7 @@ class GitData:
                             if c_inner == commit:
                                 break
                             fh.fill(c_inner, cfile.b_path)
+                        fh.fill(commit, cfile.b_path)
                         file_histories[cfile.b_path] = fh
                     else:
                         fh.fill(commit, cfile.b_path)
@@ -134,6 +235,8 @@ class GitData:
                             if c_inner == commit:
                                 break
                             fh.fill(c_inner, cfile.a_path)
+                        fh.fill(commit, cfile.b_path)
+                        file_histories[cfile.b_path] = fh
                     else:
                         fh.fill(commit, cfile.a_path)
 
@@ -146,6 +249,8 @@ class GitData:
                             if c_inner == commit:
                                 break
                             fh.fill(c_inner, cfile.a_path)
+                        fh.fill(commit, cfile.b_path)
+                        file_histories[cfile.b_path] = fh
                     else:
                         fh.fill(commit, cfile.b_path)
 
@@ -157,5 +262,14 @@ class GitData:
                 if not fh.file_commits.get(commit.sha):
                     fh.fill(commit)
 
-        self.file_histories = file_histories
+        for fh in file_histories.values():
+            fh.post_load()
 
+        for fh in file_histories.values():
+            all_patches = fh.all_patches
+            total_length = fh.total_length
+            for fc in fh.file_commits.values():
+                fc.get_content(all_patches, total_length) # exercise this code
+
+
+        self.file_histories = file_histories
--- rediff.py
+++ rediff.py
@@ -2,29 +2,72 @@ import click
 from collections import OrderedDict
 
 from textual.app import App, ComposeResult
-from textual.widgets import TextArea, Static
-from textual.containers import HorizontalScroll
+from textual.containers import HorizontalScroll, Vertical
+from textual.widget import Widget
+from textual.widgets import Label, Markdown
 
 import db
+from filediffview import FileDiffView
 
 class SingleFileAllCommits(HorizontalScroll):
     def __init__(self, file_history):
         super().__init__()
         self.file_history = file_history
         self.file_views = OrderedDict()
+        self._curr_pane = 0
 
     def compose(self):
         for file_commit in self.file_history.file_commits.values():
-            self.file_views[file_commit.sha] = FileDiffView(
+            self.file_views[file_commit.sha] = CommitFilePane(
                 file_commit,
+                self.file_history,
             )
+
         yield HorizontalScroll(*self.file_views.values())
 
-class FileDiffView(TextArea):
-    def __init__(self, file_commit):
+    def on_mount(self):
+        self.focus_pane(self._curr_pane)
+
+    def focus_pane(self, next_pane):
+        num_panes = len(self.file_views.keys())
+        next_pane = min(max(0, next_pane), num_panes-1)
+        for i, commit_file_pane in enumerate(self.file_views.values()):
+            if i == next_pane:
+                commit_file_pane.fv.show_cursor = True
+                commit_file_pane.fv.focus()
+            else:
+                commit_file_pane.fv.show_cursor = False
+        self._curr_pane = next_pane
+
+    def on_file_diff_view_parent_command(self, command):
+        print(f"here 1: {command.cmd}")
+        if command.cmd == "focus_pane_left":
+            print("move left")
+            self.focus_pane(self._curr_pane - 1)
+        elif command.cmd == "focus_pane_right":
+            print("move right")
+            self.focus_pane(self._curr_pane + 1)
+        elif command.cmd == "cursor_move":
+            print("cursor down")
+            for cp in self.file_views.values():
+                cp.fv.move_cursor(command.data["location"])
+
+class CommitFilePane(Vertical):
+    def __init__(self, file_commit, file_history):
         super().__init__()
-        self.file_name = file_commit.file_name
-        self.text = file_commit.get_file_contents()
+        self.file_commit = file_commit
+        self.file_history = file_history
+
+    def compose(self):
+        yield Label(f"{self.file_commit.commit.short}\n"
+                    f"{self.file_commit.commit.summary}")
+        yield Label(f"{self.file_commit.file_name}")
+        self.fv = FileDiffView(
+                self.file_commit,
+                self.file_history.all_patches,
+                self.file_history.total_length,
+            )
+        yield self.fv
 
 class Rediff(App):
     CSS_PATH = "app.tcss"
@@ -32,13 +75,34 @@ class Rediff(App):
     def __init__(self, repo_path, base_ref):
         super().__init__()
         self.gitdata = db.GitData(repo_path, base_ref)
+        self._curr_file = 0
 
     def compose(self) -> ComposeResult:
-        self.file_views = {}
-        for file_history in [self.gitdata.get_file_history(0)]:
-            file1 = SingleFileAllCommits(file_history)
+        file_history = self.gitdata.get_file_history(self._curr_file)
+        self.file_view = SingleFileAllCommits(file_history)
+        yield self.file_view
 
-        yield file1
+    def show_file(self, file_num_):
+        num_files = len(self.gitdata.file_histories)
+        file_num = min(num_files-1, max(0, file_num_))
+        if file_num != file_num_:
+            return
+        self.file_view.remove()
+
+        file_history = self.gitdata.get_file_history(self._curr_file)
+        self.file_view = SingleFileAllCommits(file_history)
+        self.file_view.mount()
+        self._curr_file = file_num
+        self.mount(self.file_view)
+
+    def on_file_diff_view_parent_command(self, command):
+        print("here 2")
+        if command.cmd == "focus_file_prev":
+            print("prev file")
+            self.show_file(self._curr_file - 1)
+        if command.cmd == "focus_file_next":
+            print("next file")
+            self.show_file(self._curr_file + 1)
 
 @click.command()
 @click.argument('base_ref', type=str)
